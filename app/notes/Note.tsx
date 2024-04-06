f79eac6c5c06d1d9847cbb6b1f4de1e754226ca9
Bartosz Ebert <bartoszebert@gmail.com>
2024-04-06 15:28:24 +0200

    add docker and deploy on fly.io

---
+++ app/notes/Note.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import styles from "./Notes.module.css";
+
+export default function Note({ note }: any) {
+  const { id, title, content, created } = note;
+  const router = useRouter();
+
+  const deleteNote = async (id: string) => {
+    await fetch(`http://127.0.0.1:8090/api/collections/notes/records/${id}`, {
+      method: "DELETE",
+    });
+    router.refresh();
+  };
+
+  return (
+    <div className={styles.note}>
+      <Link href={`/notes/${id}`}>
+        <h2>{title}</h2>
+        <h5>{content}</h5>
+        <p>{created}</p>
+      </Link>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          deleteNote(id);
+        }}
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
